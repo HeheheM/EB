f058bcc0231a33db450fb855dcd4860c054add09
MarioGK <mariogk01@gmail.com>
2016-01-07 20:37:18 -0200

    SpellCastAIO release

---
+++ SpellCasterAIO/SpellCasterAIO/Modes/LastHit.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+
+using Settings = SpellCasterAIO.Config.Modes.LastHit;
+
+namespace SpellCasterAIO.Modes
+{
+    public sealed class LastHit : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit);
+        }
+
+        public override void Execute()
+        {
+            var target = EntityManager.MinionsAndMonsters.GetJungleMonsters().OrderBy(m => m.Health).FirstOrDefault();
+            if (target == null) return;
+
+            var targetQ =
+                EntityManager.MinionsAndMonsters.GetJungleMonsters()
+                    .OrderByDescending(m => m.Health)
+                    .FirstOrDefault(m => m.Health <= Player.Instance.GetSpellDamage(m, SpellSlot.Q));
+
+            if (Q.IsReady() && targetQ.IsValidTarget(Q.Range) && Settings.UseQ)
+            {
+                if (SpellManager.SpellNeedTarget(SpellSlot.Q))
+                {
+                    Q.Cast(targetQ);
+                }
+                else
+                {
+                    Q.Cast();
+                }
+            }
+
+            var targetW =
+                EntityManager.MinionsAndMonsters.GetJungleMonsters()
+                    .OrderByDescending(m => m.Health)
+                    .FirstOrDefault(m => m.Health <= Player.Instance.GetSpellDamage(m, SpellSlot.W));
+
+            if (W.IsReady() && target.IsValidTarget(W.Range) && Settings.UseW)
+            {
+                if (SpellManager.SpellNeedTarget(SpellSlot.W))
+                {
+                    W.Cast(targetW);
+                }
+                else
+                {
+                    W.Cast();
+                }
+            }
+
+            var targetE =
+                EntityManager.MinionsAndMonsters.GetJungleMonsters()
+                    .OrderByDescending(m => m.Health)
+                    .FirstOrDefault(m => m.Health <= Player.Instance.GetSpellDamage(m, SpellSlot.E));
+
+            if (E.IsReady() && target.IsValidTarget(E.Range) && Settings.UseE)
+            {
+                if (SpellManager.SpellNeedTarget(SpellSlot.E))
+                {
+                    E.Cast(targetE);
+                }
+                else
+                {
+                    E.Cast();
+                }
+            }
+
+            var targetR =
+                EntityManager.MinionsAndMonsters.GetJungleMonsters()
+                    .OrderByDescending(m => m.Health)
+                    .FirstOrDefault(m => m.Health <= Player.Instance.GetSpellDamage(m, SpellSlot.R));
+
+            if (R.IsReady() && target.IsValidTarget(R.Range) && Settings.UseR)
+            {
+                if (SpellManager.SpellNeedTarget(SpellSlot.R))
+                {
+                    R.Cast(targetR);
+                }
+                else
+                {
+                    R.Cast();
+                }
+            }
+        }
+    }
+}
---
+++ SpellCasterAIO/SpellCasterAIO/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Rendering;
+using Settings = SpellCasterAIO.Config.Modes.Draw;
+
+
+namespace SpellCasterAIO
+{
+    public static class Program
+    {
+        // ReSharper disable once MemberCanBePrivate.Global
+        public const string ChampName = "";
+
+        public static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName == ChampName)
+            {
+                return;
+            }
+
+            Config.Initialize();
+            SpellManager.Initialize();
+            ModeManager.Initialize();
+
+            ActivatorHu3.Activator.Init();
+
+            Drawing.OnDraw += OnDraw;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            if (Settings.DrawQ)
+            {
+                new Circle { Color = SpellManager.Q.IsReady() ? Color.DodgerBlue : Color.Red, BorderWidth = 1f, Radius = SpellManager.Q.Range }.Draw(Player.Instance.Position);
+            }
+
+            if (Settings.DrawW)
+            {
+                new Circle { Color = SpellManager.W.IsReady() ? Color.DodgerBlue : Color.Red, BorderWidth = 1f, Radius = SpellManager.W.Range }.Draw(Player.Instance.Position);
+            }
+
+            if (Settings.DrawE)
+            {
+                new Circle { Color = SpellManager.E.IsReady() ? Color.DodgerBlue : Color.Red, BorderWidth = 1f, Radius = SpellManager.E.Range }.Draw(Player.Instance.Position);
+            }
+
+            if (Settings.DrawR)
+            {
+                new Circle { Color = SpellManager.R.IsReady() ? Color.DodgerBlue : Color.Red, BorderWidth = 1f, Radius = SpellManager.R.Range }.Draw(Player.Instance.Position);
+            }
+        }
+    }
+}
