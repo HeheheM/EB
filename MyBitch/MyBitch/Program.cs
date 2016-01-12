3298deca70b8b10079f1b9f3e8b67d1f068f8408
MarioGK <mariogk01@gmail.com>
2016-01-12 18:57:46 -0200

    asdas

--- MyBitch/MyBitch/Program.cs
+++ MyBitch/MyBitch/Program.cs
@@ -18,20 +18,15 @@ namespace MyBitch
             Game.OnTick += Game_OnTick;
         }
 
-        private static int LastRun;
+        //private static int LastRun;
 
         private static void Game_OnTick(EventArgs args)
         {
-            if (LastRun > Environment.TickCount)return;
+            var place1 = new Vector3(6900f, 1806f, -189f);
+            var place2 = new Vector3(7000f, 1806f, -189f);
 
-            var place = new Vector3(6950f, 1806f, -189f);
-            var dist = Player.Instance.Distance(place);
-            if (dist > 300)
-            {
-                Chat.Say("/all Going to the position!");
-                Player.IssueOrder(GameObjectOrder.MoveTo, place);
-            }
-            LastRun = Environment.TickCount + 5000;
+            Player.IssueOrder(GameObjectOrder.MoveTo, place1);
+            Core.DelayAction(()=> Player.IssueOrder(GameObjectOrder.MoveTo, place1), 900);
         }
     }
 }
