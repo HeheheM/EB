2dcf97784a1afffd18632f3b376a971f455b615b
MarioGK <mariogk01@gmail.com>
2016-01-12 19:07:03 -0200

    asdasdas

--- MyBitch/MyBitch/Program.cs
+++ MyBitch/MyBitch/Program.cs
@@ -3,6 +3,7 @@ using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
 using SharpDX;
+using Color = System.Drawing.Color;
 
 namespace MyBitch
 {
@@ -11,32 +12,48 @@ namespace MyBitch
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += Loading_OnLoadingComplete;
-            var place1 = new Vector3(6900f, 1806f, -189f);
+            var place1 = new Vector3(6900f, 1800f, -189f);
             Player.IssueOrder(GameObjectOrder.MoveTo, place1);
         }
 
         private static void Loading_OnLoadingComplete(EventArgs args)
         {
             Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
         }
 
-        //private static int LastRun;
-        
         private static void Game_OnTick(EventArgs args)
         {
-            var place1 = new Vector3(6900f, 1806f, -189f);
-            var place2 = new Vector3(7000f, 1806f, -189f);
-
-            if (Player.Instance.Distance(place2) < 10)
-            {
-                Player.IssueOrder(GameObjectOrder.MoveTo, place1);
-            }
+            var place1 = new Vector3(6900f, 1800f, -189f);
+            var place2 = new Vector3(7900f, 1800f, -189f);
+            var place3 = new Vector3(7900f, 1700f, -189f);
+            var place4 = new Vector3(6900f, 1700f, -189f);
 
             if (Player.Instance.Distance(place1) < 10)
             {
                 Player.IssueOrder(GameObjectOrder.MoveTo, place2);
             }
-            
+
+            if (Player.Instance.Distance(place2) < 10)
+            {
+                Player.IssueOrder(GameObjectOrder.MoveTo, place3);
+            }
+
+            if (Player.Instance.Distance(place3) < 10)
+            {
+                Player.IssueOrder(GameObjectOrder.MoveTo, place4);
+            }
+
+            if (Player.Instance.Distance(place4) < 10)
+            {
+                Player.IssueOrder(GameObjectOrder.MoveTo, place1);
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            var place1 = new Vector3(6900f, 1800f, -189f);
+            Drawing.DrawCircle(place1, 60f, Color.Red);
         }
     }
 }
